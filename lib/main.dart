1750dcfed7acfc7a3dbbc9d6bd4835f8ff698191
mvfernando <you@example.com>
2023-02-26 01:37:47 +0100

    my first commit on app on

---
+++ lib/main.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: ' My app one'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter = _counter + 1;
+    });
+  }
+  void _DecrementCounter() {
+    setState(() {
+      _counter = _counter - 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+     
+    return Scaffold(
+      appBar: AppBar(
+         
+        title: Text(widget.title),
+      ),
+      body: Center(
+         
+        child: Column(
+           
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          FloatingActionButton(
+            onPressed: _incrementCounter,
+            tooltip: 'Increment',
+            child: const Icon(Icons.add),
+          ),
+          FloatingActionButton(
+            onPressed: _DecrementCounter,
+            tooltip: 'Decrement',
+            child: const Icon(Icons.minimize),
+            backgroundColor: Colors.red,
+          ),
+        ],
+        
+      ),
+    );
+  }
+}
